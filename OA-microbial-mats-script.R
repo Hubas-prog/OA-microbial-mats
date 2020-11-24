6c84350317e35711189b337912f76c83bbd77f2d
Hubas-prog <cedrichubas@mageos.com>
2020-11-24 17:59:38 +0100

    format R script to general dataset

--- OA-microbial-mats-script.R
+++ OA-microbial-mats-script.R
@@ -112,20 +112,36 @@ title("variables-P8")
 # Pigment Analysis (short list)
 ##########################################
 
-# Upload pigment data
-all.pig.data <- read.table("Pourcentages.txt",h=T) # all pigments
-all.pig.data$treatment<-factor(gsub(pattern = "PH","pH",all.pig.data$treatment))
-short.pig.data <- read.table("POURCENT.txt",h=T) # pigments grouping
-all.pig.data$treatment
-
-# Data Homogeneization
-all.pig.data2 <- all.pig.data[all.pig.data$treatment!="Ctr.bis",]
-short.pig.data2 <- short.pig.data[all.pig.data$treatment!="Ctr.bis",]
-short.pig.data2$time<-all.pig.data2$time
-short.pig.data2$treatment<-all.pig.data2$treatment
+all.pig.data <- no.na.data[,c(1:4,15:51)]
+names(all.pig.data)
+apply(all.pig.data[,-c(1:4)],1,sum)
+short.pig.data <- data.frame(
+  Cy=all.pig.data$"aCy"+all.pig.data$"bCy",
+  Al=all.pig.data$"Al",
+  Caroten=all.pig.data$"BB.Car"+all.pig.data$"BE.Car",
+  UCar=all.pig.data$"Car1"+all.pig.data$"Car2",
+  Ca=all.pig.data$"Ca"+all.pig.data$"Ca.allo"+all.pig.data$"Ca.epi",
+  Ca.iso=all.pig.data$"Ca.iso1"+all.pig.data$"Ca.iso2"+all.pig.data$"Ca.iso3"+all.pig.data$"Ca.iso4"+all.pig.data$"Ca.iso5"+all.pig.data$"Ca.iso7",
+  BCa=all.pig.data$"Bca",
+  Cb=all.pig.data$"Cb",
+  Cc=all.pig.data$"Cc2",
+  Cda=all.pig.data$"Cda",
+  Ct=all.pig.data$"Ct"+all.pig.data$"Ct.iso",
+  Ec=all.pig.data$"Ec",
+  F=all.pig.data$F+all.pig.data$"F.iso1",
+  L=all.pig.data$L+all.pig.data$"L.iso1"+all.pig.data$"L.iso2",
+  My=all.pig.data$"My.iso1"+all.pig.data$"My.iso2"+all.pig.data$"My.iso3"+all.pig.data$"My.iso4",
+  O=all.pig.data$"O",
+  Pda=all.pig.data$"Pda.1",
+  Pha=all.pig.data$"Pha",
+  Pya=all.pig.data$"Pya",
+  Z=all.pig.data$"Z"
+)
+short.pig.data$treatment<-factor(all.pig.data$treatment)
+short.pig.data$time<-factor(all.pig.data$time)
 
 # make lists
-short.pig.list<-split(short.pig.data2,all.pig.data2$time)
+short.pig.list<-split(short.pig.data,short.pig.data$time)
 
 #######################################
 # Create a function for short.pig dataset
@@ -148,9 +164,10 @@ make.subplot.short.pig<-function(data){
 # make plots
 layout(matrix(c(1,3,5,7,2,4,6,8,9,9,9,9),ncol=3))
 par(mar=c(2,1,1,1))
-lapply(all.pig.list,make.subplot.short.pig)
-X<-dudi.pca(short.pig.data2[short.pig.data2$time=="P8",1:20],scannf=F,nf=2)
-Fa<-factor(short.pig.data2[short.pig.data2$time=="P8",]$treatment)
+names(short.pig.data)
+lapply(short.pig.list,make.subplot.short.pig)
+X<-dudi.pca(short.pig.data[short.pig.data$time=="P8",1:20],scannf=F,nf=2)
+Fa<-factor(short.pig.data[short.pig.data$time=="P8",]$treatment)
 coord<-bca(X,Fa,scannf=F,nf=2)$co
 plot(coord,type="n")
 arrows(x0=0,y0=0,x1=coord$Comp1,y1=coord$Comp2,col="lightgrey")
@@ -160,13 +177,12 @@ abline(v=0,lty="dashed")
 title("variables-P8")
 
 
-
 ##########################################
 # Focus on Chlorophyll a "derivatives"like" pigments
 ##########################################
 
 # boxplots
-box.ca.iso<-ggplot(short.pig.data2,
+box.ca.iso<-ggplot(short.pig.data,
        aes(x = time,
            y= Ca.iso,
            col=treatment))+
@@ -175,7 +191,7 @@ box.ca.iso<-ggplot(short.pig.data2,
   xlab("Time")+
   theme_bw()
 
-box.ca<-ggplot(short.pig.data2,
+box.ca<-ggplot(short.pig.data,
        aes(x = time,
            y= Ca,
            col=treatment))+
@@ -184,7 +200,7 @@ box.ca<-ggplot(short.pig.data2,
   xlab("Time")+
   theme_bw()
 
-box.pha<-ggplot(short.pig.data2,
+box.pha<-ggplot(short.pig.data,
        aes(x = time,
            y= Pha,
            col=treatment))+
@@ -196,9 +212,9 @@ box.pha<-ggplot(short.pig.data2,
 plot_grid(box.ca,box.ca.iso,box.pha,labels=c("a","b","c"),nrow=3)
 
 # scatter plots
-time.j<-c(1,8,15,23,30,37,44,51)[short.pig.data2$time]
+time.j<-c(1,8,15,23,30,37,44,51)[short.pig.data$time]
 
-smooth_iso<-ggplot(short.pig.data2,
+smooth_iso<-ggplot(short.pig.data,
        aes(x = time.j,
            y= Ca.iso,
            col=treatment))+
@@ -208,7 +224,7 @@ smooth_iso<-ggplot(short.pig.data2,
   xlab("Time")+
   theme_bw()
 
-smooth_ca<-ggplot(short.pig.data2,
+smooth_ca<-ggplot(short.pig.data,
        aes(x = time.j,
            y= Ca,
            col=treatment))+
@@ -218,7 +234,7 @@ smooth_ca<-ggplot(short.pig.data2,
   xlab("Time")+
   theme_bw()
 
-smooth_Pha<-ggplot(short.pig.data2,
+smooth_Pha<-ggplot(short.pig.data,
                   aes(x = time.j,
                       y= Pha,
                       col=treatment))+
@@ -228,10 +244,10 @@ smooth_Pha<-ggplot(short.pig.data2,
   xlab("Time")+
   theme_bw()
 
-plot_grid(smooth_ca,smooth_iso,smooth_Pha,labels=c("a","b","c"),nrow=1)
+plot_grid(smooth_ca,smooth_iso,smooth_Pha,labels=c("a","b","c"),nrow=3)
 
 # Ratio Chlorophyll a like vs. true Chlorophyll a
-ggplot(short.pig.data2,
+ggplot(short.pig.data,
                   aes(x = time,
                       y= Ca.iso/Pha,
                       col=treatment))+
@@ -239,3 +255,4 @@ ggplot(short.pig.data2,
   ylab("metalation ratio")+
   xlab("Time")+
   theme_bw()
+
