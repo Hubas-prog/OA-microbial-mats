b273276301f19d9ed9dcf97201f08f1ea4b7a30d
Hubas-prog <cedrichubas@mageos.com>
2020-11-24 16:15:46 +0100

    tidying up pigment script

--- OA-microbial-mats-script.R
+++ OA-microbial-mats-script.R
@@ -23,15 +23,100 @@ library(vegan)
 # palettes
 my.palette <- colorRampPalette(c("#DE4064","#FFAA5A","#FEEB94","#06D6A0","#4B7696"))
 
+##########################################
+# BC-MFA Analysis
+##########################################
+
+#### Upload data for MFA
+all.data <- read.csv("Physico-chem_MESO_modif.csv",sep=";",h=T)
+no.na.data <- na.omit(all.data)
+names(no.na.data)
+
+# split dataset according to sampling time
+no.na.data.split<-split(no.na.data,factor(no.na.data$time))
+
+#######################################
+# Create a custom function for BC-MFA analysis
+make.BC.MFA<-function(data){
+  
+  # separate matrix
+  enviro <- data[,5:14]
+  pigments <- data[,15:51]
+  eps <- data[,52:55]
+  blocs <- c(dim(enviro)[2],
+             dim(pigments)[2],
+             dim(eps)[2]
+  )
+  
+  # make dataset
+  mfa.data <- data.frame(
+    enviro,
+    pigments,
+    eps)
+  
+  # perform Mltiple factor Analysis (MFA)
+  res.mfa <- dudi.pca(mfa.data,
+                      col.w = rep(c(1/dudi.pca(enviro,scannf=F,nf=2)$eig[1],
+                                    rep(1/dudi.pca(pigments,scannf=F,nf=2)$eig[1]),
+                                    rep(1/dudi.pca(eps,scannf=F,nf=2)$eig[1])),blocs),
+                      scannf=F,
+                      nf=2)
+  
+  # create control variable (must be factor)
+  fac <- factor(data$treatment)
+  
+  # perform between class analysis on MFA results
+  res.bca <- bca(res.mfa, fac, scannf = FALSE, nf = 2)
+  plot(res.bca$ls,
+       pch=21,
+       col="white",
+       bg=my.palette(length(levels(fac)))[fac],
+       cex=3)
+  ordicluster(res.bca$ls,hclust(vegdist(data[,5:55],"euclidean"),"average",))
+  randtest(res.bca)
+}
+
+# End onf custom BC-MFA function
+#######################################
+
+# Apply custom BC-MFA function on each sampling date 
+layout(matrix(c(1,3,5,7,2,4,6,8,9,9,9,9),ncol=3))
+par(mar=c(2,1,1,1))
+# plot individuals
+lapply(no.na.data.split,make.BC.MFA)
+# plot variables for a chosen sampling time (here "P8")
+X<-dudi.pca(no.na.data.split$P8[,5:55],scannf=F,nf=2)
+Fa<-factor(no.na.data.split$P8$treatment)
+coord<-bca(X,Fa,scannf=F,nf=2)$co
+plot(coord,type="n")
+arrows(x0=0,
+       y0=0,
+       x1=coord$Comp1,
+       y1=coord$Comp2,
+       col='lightgrey')
+bloc.factor<-factor(rep(c("enviro","pigment","EPS"),c(10,37,4)))
+text(x=coord$Comp1,
+     y=coord$Comp2,
+     rownames(coord),
+     cex=1,
+     col=rainbow(3)[bloc.factor])
+abline(h=0,lty="dashed")
+abline(v=0,lty="dashed")
+title("variables-P8")
+
+
+
+
+
+##########################################
+# Pigment Analysis (short list)
+##########################################
+
 # Upload pigment data
 all.pig.data <- read.table("Pourcentages.txt",h=T) # all pigments
 all.pig.data$treatment<-factor(gsub(pattern = "PH","pH",all.pig.data$treatment))
 short.pig.data <- read.table("POURCENT.txt",h=T) # pigments grouping
 all.pig.data$treatment
-
-##########################################
-# Pigment Analysis
-##########################################
 
 # Data Homogeneization
 all.pig.data2 <- all.pig.data[all.pig.data$treatment!="Ctr.bis",]
@@ -40,23 +125,10 @@ short.pig.data2$time<-all.pig.data2$time
 short.pig.data2$treatment<-all.pig.data2$treatment
 
 # make lists
-all.pig.list<-split(all.pig.data2,all.pig.data2$time)
 short.pig.list<-split(short.pig.data2,all.pig.data2$time)
 
-# function for all.pig dataset
-make.subplot.all.pig<-function(data){
-  res.pca <- dudi.pca(data[,1:37],scannf=F,nf=2)
-  factor<-factor(data[,which(names(data)=="treatment")])
-  res.bca <- bca(res.pca, factor, scannf = FALSE, nf = 2)
-  plot(res.bca$ls,
-       pch=21,
-       col="white",
-       bg=my.palette(length(levels(factor)))[factor],
-       cex=3)
-  ordicluster(res.bca$ls,hclust(vegdist(data[,1:37],"bray"),"ward.D2",))
-}
-
-# function for short.pig dataset
+#######################################
+# Create a function for short.pig dataset
 make.subplot.short.pig<-function(data){
   res.pca <- dudi.pca(data[,1:20],scannf=F,nf=2)
   factor<-factor(data[,which(names(data)=="treatment")])
@@ -70,35 +142,31 @@ make.subplot.short.pig<-function(data){
   
 }
 
+# End of custom function
+#######################################
+
 # make plots
 layout(matrix(c(1,3,5,7,2,4,6,8,9,9,9,9),ncol=3))
-par(mar=c(0,0,0,0))
-lapply(all.pig.list,make.subplot.all.pig)
-X<-dudi.pca(all.pig.data2[all.pig.data2$time=="P6",1:37],scannf=F,nf=2)
-Fa<-factor(all.pig.data2[all.pig.data2$time=="P6",]$treatment)
-coord<-bca(X,Fa,scannf=F,nf=2)$co
-plot(coord,type="n")
-arrows(x0=0,y0=0,x1=coord$Comp1,y1=coord$Comp2,col="lightgrey")
-text(x=coord$Comp1,y=coord$Comp2,rownames(coord),cex=1.5)
-abline(h=0,lty="dashed")
-abline(v=0,lty="dashed")
-
-layout(matrix(c(1,3,5,7,2,4,6,8,9,9,9,9),ncol=3))
-par(mar=c(0,0,0,0))
+par(mar=c(2,1,1,1))
 lapply(all.pig.list,make.subplot.short.pig)
-X<-dudi.pca(short.pig.data2[short.pig.data2$time=="P6",1:20],scannf=F,nf=2)
-Fa<-factor(short.pig.data2[short.pig.data2$time=="P6",]$treatment)
+X<-dudi.pca(short.pig.data2[short.pig.data2$time=="P8",1:20],scannf=F,nf=2)
+Fa<-factor(short.pig.data2[short.pig.data2$time=="P8",]$treatment)
 coord<-bca(X,Fa,scannf=F,nf=2)$co
 plot(coord,type="n")
 arrows(x0=0,y0=0,x1=coord$Comp1,y1=coord$Comp2,col="lightgrey")
 text(x=coord$Comp1,y=coord$Comp2,rownames(coord),cex=1.5)
 abline(h=0,lty="dashed")
 abline(v=0,lty="dashed")
+title("variables-P8")
 
 
-# Focus on Chlorophyll a derivatives
 
-ggplot(short.pig.data2,
+##########################################
+# Focus on Chlorophyll a "derivatives"like" pigments
+##########################################
+
+# boxplots
+box.ca.iso<-ggplot(short.pig.data2,
        aes(x = time,
            y= Ca.iso,
            col=treatment))+
@@ -107,6 +175,27 @@ ggplot(short.pig.data2,
   xlab("Time")+
   theme_bw()
 
+box.ca<-ggplot(short.pig.data2,
+       aes(x = time,
+           y= Ca,
+           col=treatment))+
+  geom_boxplot()+
+  ylab("Chlorophyll a (%)")+
+  xlab("Time")+
+  theme_bw()
+
+box.pha<-ggplot(short.pig.data2,
+       aes(x = time,
+           y= Pha,
+           col=treatment))+
+  geom_boxplot()+
+  ylab("Pheophytin a (%)")+
+  xlab("Time")+
+  theme_bw()
+
+plot_grid(box.ca,box.ca.iso,box.pha,labels=c("a","b","c"),nrow=3)
+
+# scatter plots
 time.j<-c(1,8,15,23,30,37,44,51)[short.pig.data2$time]
 
 smooth_iso<-ggplot(short.pig.data2,
@@ -116,16 +205,6 @@ smooth_iso<-ggplot(short.pig.data2,
   geom_point()+
   geom_smooth(aes(fill=treatment),method = "loess")+
   ylab("Chlorophyll a derivatives (%)")+
-  xlab("Time")+
-  theme_bw()
-
-# Focus on Chlorophyll a
-ggplot(short.pig.data2,
-       aes(x = time,
-           y= Ca,
-           col=treatment))+
-  geom_boxplot()+
-  ylab("Chlorophyll a (%)")+
   xlab("Time")+
   theme_bw()
 
@@ -139,16 +218,6 @@ smooth_ca<-ggplot(short.pig.data2,
   xlab("Time")+
   theme_bw()
 
-# Focus on Pheophytin a
-ggplot(short.pig.data2,
-       aes(x = time,
-           y= Pha,
-           col=treatment))+
-  geom_boxplot()+
-  ylab("Pheophytin a (%)")+
-  xlab("Time")+
-  theme_bw()
-
 smooth_Pha<-ggplot(short.pig.data2,
                   aes(x = time.j,
                       y= Pha,
@@ -159,9 +228,9 @@ smooth_Pha<-ggplot(short.pig.data2,
   xlab("Time")+
   theme_bw()
 
-# plot grid
 plot_grid(smooth_ca,smooth_iso,smooth_Pha,labels=c("a","b","c"),nrow=3)
 
+# Ratio Chlorophyll a like vs. true Chlorophyll a
 ggplot(short.pig.data2,
                   aes(x = time,
                       y= Ca.iso/Pha,
@@ -170,160 +239,3 @@ ggplot(short.pig.data2,
   ylab("metalation ratio")+
   xlab("Time")+
   theme_bw()
-
-##########################################################
-#### Unsupervised analysis : multiple factor analysis (MFA)
-###########################################################
-
-#### Upload data for MFA
-all.data <- read.csv("Table_physicochem_mesocosm - Feuille 1.csv",dec=",",h=T)
-no.na.data <- na.omit(all.data)
-names(no.na.data)
-
-
-# extraction des k tables
-enviro <- no.na.data[,5:14]
-pigments <- no.na.data[,15:51]
-eps <- no.na.data[,52:55]
-blocs <- c(dim(enviro)[2],
-           dim(pigments)[2],
-           dim(eps)[2]
-)
-
-# jeu de données
-pca.data <- data.frame(
-  enviro,
-  pigments,
-  eps)
-
-# changement des poids
-res.mfa <- dudi.pca(pca.data,col.w = rep(c(1/dudi.pca(enviro,scannf=F,nf=2)$eig[1],
-                                           rep(1/dudi.pca(pigments,scannf=F,nf=2)$eig[1]),
-                                           rep(1/dudi.pca(eps,scannf=F,nf=2)$eig[1])),blocs),
-                    scannf=F,nf=2)
-
-plot(res.mfa$li,main="MFA")
-s.class(res.mfa$li,
-        as.factor(paste(no.na.data$week,no.na.data$treatment)),
-        col=my.palette(24),add.plot=T)
-
-
-
-##########################################################
-#### Supervised analysis : analyse discriminate PPLS-DA
-##########################################################
-
-# transformation du facteur en variable indicatrice
-var.ind <- dummy(paste(no.na.data$week,no.na.data$treatment,sep=""))
-fac <- as.factor(paste(no.na.data$week,no.na.data$treatment,sep=""))
-
-# transformation racine carrée
-DATA.M <- as.matrix(decostand(pca.data,"hellinger"))
-
-# modèle Canonical Powered Partial least squared (PLS) regression
-PLSDA<-cppls(var.ind ~ DATA.M, ncomp=20) 
-summary (PLSDA)
-attributes(PLSDA)
-
-par(mfrow=c(2,1))
-MVA.plot(PLSDA, "scores", fac=fac, cex = 0.8, col=my.palette(length(levels(fac))))
-MVA.plot(PLSDA, "corr", thres = 0, fac=rep(c("enviro","pigments","eps"),blocs), arrows = FALSE, cex = 0.7,intcircle = 0.5,points=F,col=rainbow(3))
-
-# cross model validation (2CV) with different PLS analyses = Classification error rate
-valid<-MVA.cmv(DATA.M, fac ,repet=50,ncomp=6,model="PPLS-DA",crit.inn="NMC")
-
-# Test différences entre groupes (si non significatif, absolument test 2 a 2)
-testglobal <-MVA.test(DATA.M, fac, model="PPLS-DA",cmv=TRUE,ncomp=6,kout=7,kinn=6)
-
-# permet de voir si groupes sont différents 2 a 2 (étape très longue si grand nombre de variables)#
-test2a2 <-pairwise.MVA.test(DATA.M, fac ,model="PPLS-DA",cmv=TRUE,ncomp=6,kout=5,kinn=4)#
-
-##########################################################
-#### Supervised analysis 2 : between-class analysis
-##########################################################
-
-# BCA interaction
-between.interaction <- bca(res.mfa,
-                           as.factor(paste(no.na.data$week)),
-                           scannf=F,nf=2)
-between.interaction$ratio
-randtest(between.interaction)
-plot(between.interaction)
-
-##########################################################
-#### Full analysis : P8
-##########################################################
-
-# extraction des k tables
-enviro.P8 <- no.na.data[no.na.data$week=="P8",5:14]
-pigments.P8 <- no.na.data[no.na.data$week=="P8",15:51]
-eps.P8 <- no.na.data[no.na.data$week=="P8",52:55]
-blocs.P8 <- c(dim(enviro.P8)[2],
-           dim(pigments.P8)[2],
-           dim(eps.P8)[2]
-)
-
-#  standardisation par unité d'inertie
-pca.data.P8 <- data.frame(
-  enviro.P8,
-  pigments.P8,
-  eps.P8)
-
-# MFA
-res.mfa.P8 <- dudi.pca(pca.data.P8,col.w = rep(c(1/dudi.pca(enviro.P8,scannf=F,nf=2)$eig[1],
-                                                 rep(1/dudi.pca(pigments.P8,scannf=F,nf=2)$eig[1]),
-                                                 rep(1/dudi.pca(eps.P8,scannf=F,nf=2)$eig[1])),blocs.P8),
-                       scannf=F,nf=2)
-
-plot(res.mfa.P8$li,main="MFA")
-s.class(res.mfa.P8$li,
-        as.factor(paste(no.na.data$week[no.na.data$week=="P8"],
-                        no.na.data$treatmen[no.na.data$week=="P8"])),
-        col=my.palette(4),add.plot=T)
-
-
-# transformation du facteur en variable indicatrice
-var.ind.P8 <- dummy(paste(no.na.data$week,no.na.data$treatment,sep="")[no.na.data$week=="P8"])
-fac.P8 <- as.factor(paste(no.na.data$week,no.na.data$treatment,sep="")[no.na.data$week=="P8"])
-
-# transformation racine carrée
-DATA.M.P8 <- as.matrix(decostand(mfa.data.P8,"hellinger"))
-
-# modèle Canonical Powered Partial least squared (PLS) regression
-PLSDA.P8<-cppls(var.ind.P8 ~ DATA.M.P8) 
-
-par(mfrow=c(2,1))
-MVA.plot(PLSDA.P8, "scores", fac=fac.P8, cex = 0.8, col=my.palette(length(levels(fac.P8))))
-MVA.plot(PLSDA.P8, "corr", thres = 0, fac=rep(c("enviro","pigments","eps"),blocs), arrows = FALSE, cex = 0.7,intcircle = 0.5,points=F,col=rainbow(3))
-
-# BCA treatment
-between.treatment.P8 <- bca(res.mfa.P8,fac.P8,scannf=F,nf=2)
-between.treatment.P8$ratio
-
-plot(between.treatment.P8$ls,col=my.palette(length(levels(fac.P8)))[fac.P8])
-s.class(between.treatment.P8$ls,fac.P8,
-        add.plot=T,
-        col=my.palette(length(levels(fac.P8))),
-        axesell = F,
-        cellipse=0
-        )
-plot(between.treatment.P8$co,type="n")
-arrows(x0 = 0,
-       y0=0,
-       x1=between.treatment.P8$co[,1],
-       y1=between.treatment.P8$co[,2],
-       col=alpha(rainbow(3)[as.factor(rep(c("enviro","pigments","eps"),blocs))],0.5),
-       length=0.2
-       )
-text(x=between.treatment.P8$co[,1],
-     y=between.treatment.P8$co[,2],
-     rownames(between.treatment.P8$co),
-     col=rainbow(3)[as.factor(rep(c("enviro","pigments","eps"),blocs))],
-     cex=0.7
-     )
-abline(h=0,col="grey")
-abline(v=0,col="grey")
-
-randtest(between.treatment.P8)
-plot(between.treatment.P8)
-
